5f4fae28c476b1d5467b5bcf8a58b904c539a845
shinmj <gkdlqkek@gmail.com>
2023-07-05 01:30:29 +0900

    feat: resuable widgets

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 void main() {
   runApp(App());
@@ -9,53 +10,51 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //테마 선택 : MaterialApp 혹은 CupertinoApp 
+    //테마 선택 : MaterialApp 혹은 CupertinoApp
     return MaterialApp(
-        home: Scaffold(
-          backgroundColor: const Color(0xff181818),
-          body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+      home: Scaffold(
+        backgroundColor: const Color(0xff181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hey, Selena', 
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800
-                            ),
-                          ),
-                          Text(
-                            'Welcome back',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 18
-                            ),
-                          ),
-                        ],
+                      const Text(
+                        'Hey, Selena',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
                       ),
                     ],
-                ),
-                const SizedBox(
-                  height: 120,
-                ),
-                Text('Total Balance',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withOpacity(0.8)
                   ),
-                ),
-                const SizedBox(
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                    fontSize: 22, color: Colors.white.withOpacity(0.8)),
+              ),
+              const SizedBox(
                 height: 5,
               ),
               const Text(
@@ -65,36 +64,21 @@ class App extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
-              ), 
+              ),
               const SizedBox(
                 height: 30,
               ),
               Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 20,
-                        horizontal: 50,
-                      ),
-                      child: Text(
-                        'Transfer',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  )
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Button(text: 'Transfer', bgColor: Colors.amber, textColor: Colors.black,),
+                  Button(text: 'Request', bgColor: Color(0xff1f2123), textColor: Colors.white,),
                 ],
               )
-              ],
-            ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
