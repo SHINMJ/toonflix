6f9e07b60de98a4de53d2f3d0ac4ea63560a50f3
shinmj <gkdlqkek@gmail.com>
2023-07-05 00:26:55 +0900

    feat: header

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -12,13 +12,42 @@ class App extends StatelessWidget {
     //테마 선택 : MaterialApp 혹은 CupertinoApp 
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello Flutter!!'),
-          ),
-            body: Center(
-              child: Text('Hello world!!'),
+          backgroundColor: Color(0xff181818),
+          body: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Hey, Selena', 
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800
+                            ),
+                          ),
+                          Text(
+                            'Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18
+                            ),
+                          ),
+                        ],),
+                    ],
+                ),
+              ],
             ),
           ),
+        ),
     );
   }
 }
