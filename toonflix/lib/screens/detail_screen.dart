11bfc54dc1ebd95720be592206e0440a5296cfae
shinmj <gkdlqkek@gmail.com>
2023-07-09 15:48:01 +0900

    feat: favorites

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/services/api_service.dart';
@@ -16,8 +17,26 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  static const String likedToonsKey = 'likedToons';
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList(likedToonsKey);
+    print(likedToons);
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+      }
+    } else {
+      await prefs.setStringList(likedToonsKey, []);
+    }
+  }
 
   @override
   void initState() {
@@ -25,7 +44,24 @@ class _DetailScreenState extends State<DetailScreen> {
     setState(() {
       webtoon = ApiService.getToonById(widget.id);
       episodes = ApiService.getLatesEpisodesById(widget.id);
+      initPrefs();
     });
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList(likedToonsKey);
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+
+      await prefs.setStringList(likedToonsKey, likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
   }
 
   @override
@@ -33,6 +69,13 @@ class _DetailScreenState extends State<DetailScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: Icon(
+                isLiked ? Icons.favorite : Icons.favorite_outline_outlined),
+          ),
+        ],
         title: Text(
           widget.title,
           style: const TextStyle(
@@ -45,7 +88,7 @@ class _DetailScreenState extends State<DetailScreen> {
         elevation: 2,
       ),
       body: SingleChildScrollView(
-          child: Padding(
+        child: Padding(
           padding: const EdgeInsets.all(50),
           child: Column(
             children: [
@@ -120,7 +163,10 @@ class _DetailScreenState extends State<DetailScreen> {
                     return Column(
                       children: [
                         for (var episode in snapshot.data!)
-                          Episode(episode: episode, webtoonId: widget.id,)
+                          Episode(
+                            episode: episode,
+                            webtoonId: widget.id,
+                          )
                       ],
                     );
                   }
